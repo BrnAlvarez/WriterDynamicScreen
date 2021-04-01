61b4726880b8087ba7fdaf057b7aebaa931efdae
BrnAlvarez <48591745+BrnAlvarez@users.noreply.github.com>
2021-03-31 21:22:23 -0300

    Day 18 #100DaysofCode - Add function for read file local, text plain, element DOM, input, textarea, and change object parameters

--- WriterDynamicScreen-beta-0.1.js
+++ WriterDynamicScreen-beta-0.1.js
@@ -1,27 +1,40 @@
 (function (window, document) {
-    var _fx = function () {
-        var fnx = {
-            write: function (origin, element, clean = false) {
+    let _fx = function () {
+        let fnx = {
+            write: function (origin, element) {
 
                 if (!origin || typeof origin === "object")
                     return;
 
-                if (!element || typeof element === "object") {
-                    if (!element.element)
-                        return;
-                    element = element.element;
+                if (!element)
+                    return false;
+                if (typeof element === "object") {
+
+                    if (!element.timeSpace)
+                        element.timeSpace = 60;
+                    if (!element.timeBreakLine)
+                        element.timeBreakLine = 150;
+                    if (!element.timeWriter)
+                        element.timeWriter = 50;
+                    let timeSpace = element.timeSpace;
+                    let timeBreakLine = element.timeBreakLine;
+                    let timeWriter = element.timeWriter;
+                    let element = element.element;
+                    console.log(timeSpace, timeBreakLine, timeWriter, element);
+                } else {
+                    return;
                 }
                 if (document.body.contains(document.querySelector(origin))) {
 
                     if (clean)
                         document.querySelector(element).innerHTML = '';
                     if (document.querySelector(origin).getAttribute('type') == 'file') {
-                        var fileInput = document.querySelector(origin);
+                        let fileInput = document.querySelector(origin);
 
                         //falta validar que contenga un archivo
 
-                        var filePath = fileInput.value;
-                        var allowedExtensions = /(.txt)$/i;
+                        let filePath = fileInput.value;
+                        let allowedExtensions = /(.txt)$/i;
                         if (!allowedExtensions.exec(filePath)) {
                             fileInput.value = '';
                             return false;
@@ -29,21 +42,21 @@
                             //Image preview
                             if (fileInput.files && fileInput.files[0]) {
 
-                                var lector = new FileReader();
+                                let lector = new FileReader();
                                 lector.onload = function (e) {
-                                    var texto = e.target.result;
+                                    let texto = e.target.result;
                                     texto = texto.toUpperCase();
-                                    var timer = 1000;
+                                    let timer = 1000;
                                     contieneCallback(texto, timer, function (respuesta) {
-                                        for (var i = 0; i < respuesta.length; i++) {
-                                            var keyText = respuesta.substr(i, 1);
-                                            var numberOfLineBreaks = (keyText.match(/\n/g) || []).length;
+                                        for (let i = 0; i < respuesta.length; i++) {
+                                            let keyText = respuesta.substr(i, 1);
+                                            let numberOfLineBreaks = (keyText.match(/\n/g) || []).length;
                                             if (keyText == " ")
-                                                timer = timer + 150;
+                                                timer = timer + timeSpace;
                                             else if (numberOfLineBreaks >= 1)
-                                                timer = timer + 200;
+                                                timer = timer + timeBreakLine;
                                             else
-                                                timer = timer + 100;
+                                                timer = timer + timeWriter;
                                             contieneCallback(keyText, timer, function (respuesta2) {
                                                 ejecutar(element, respuesta2);
                                             });
@@ -56,19 +69,19 @@
                             }
                         }
                     } else {
-                        var texto = document.querySelector(origin).value;
+                        let texto = document.querySelector(origin).value;
                         texto = texto.toUpperCase();
-                        var timer = 1000;
+                        let timer = 1000;
                         contieneCallback(texto, timer, function (respuesta) {
-                            for (var i = 0; i < respuesta.length; i++) {
-                                var keyText = respuesta.substr(i, 1);
-                                var numberOfLineBreaks = (keyText.match(/\n/g) || []).length;
+                            for (let i = 0; i < respuesta.length; i++) {
+                                let keyText = respuesta.substr(i, 1);
+                                let numberOfLineBreaks = (keyText.match(/\n/g) || []).length;
                                 if (keyText == " ")
-                                    timer = timer + 150;
+                                    timer = timer + timeSpace;
                                 else if (numberOfLineBreaks >= 1)
-                                    timer = timer + 200;
+                                    timer = timer + timeBreakLine;
                                 else
-                                    timer = timer + 100;
+                                    timer = timer + timeWriter;
                                 contieneCallback(keyText, timer, function (respuesta2) {
                                     ejecutar(element, respuesta2);
                                 });
@@ -76,19 +89,82 @@
                         });
                     }
                 } else {
-                    var texto = origin;
+
+                    try {
+                        // Check for the various File API support.
+                        if (window.File && window.FileReader && window.FileList && window.Blob) {
+                            // Great success! All the File APIs are supported.
+                        } else {
+                            console.log('The File APIs are not fully supported in this browser.');
+                        }
+                        let origin2 = origin;
+                        origin2 = origin2.split(/(\/\/)/)[0];
+                        origin2 = origin2.split(/(.txt)/);
+                        origin2 = origin2[0] + origin2[1];
+                        let regex = /^((\/|(\\?))[\w .]+)+\.txt$/i;
+                        if (regex.test(origin2)) {
+                            let filePath = origin2;
+                            let allowedExtensions = /(.txt)$/i;
+                            if (!allowedExtensions.exec(filePath)) {
+
+                                //no es un archivo, se debe setear el texto
+
+                            } else {
+                                let rawFile;
+                                if (window.XMLHttpRequest) {
+                                    rawFile = new XMLHttpRequest();
+                                } else {
+                                    rawFile = new ActiveXObject("Microsoft.XMLHTTP");
+                                }
+                                // origin2 = origin2.replace(" ", "%20");
+                                rawFile.open("GET", origin2, true);
+                                rawFile.onreadystatechange = function () {
+                                    if (rawFile.readyState === 4) {
+                                        if (rawFile.status === 200 || rawFile.status == 0) {
+                                            let allText = rawFile.responseText;
+                                            let texto = allText;
+                                            texto = texto.toUpperCase();
+                                            let timer = 1000;
+                                            contieneCallback(texto, timer, function (respuesta) {
+                                                for (let i = 0; i < respuesta.length; i++) {
+                                                    let keyText = respuesta.substr(i, 1);
+                                                    let numberOfLineBreaks = (keyText.match(/\n/g) || []).length;
+                                                    if (keyText == " ")
+                                                        timer = timer + timeSpace;
+                                                    else if (numberOfLineBreaks >= 1)
+                                                        timer = timer + timeBreakLine;
+                                                    else
+                                                        timer = timer + timeWriter;
+                                                    contieneCallback(keyText, timer, function (respuesta2) {
+                                                        ejecutar(element, respuesta2);
+                                                    });
+                                                }
+                                            });
+                                        }
+                                    }
+                                }
+                                rawFile.send(null);
+                                return;
+                            }
+                        }
+
+                    } catch (error) {
+                        console.log(error);
+                    }
+
+                    let texto = origin;
                     texto = texto.toUpperCase();
-                    var timer = 1000;
+                    let timer = 1000;
                     contieneCallback(texto, timer, function (respuesta) {
-                        for (var i = 0; i < respuesta.length; i++) {
-                            var keyText = respuesta.substr(i, 1);
-                            var numberOfLineBreaks = (keyText.match(/\n/g) || []).length;
+                        for (let i = 0; i < respuesta.length; i++) {
+                            let keyText = respuesta.substr(i, 1);
+                            let numberOfLineBreaks = (keyText.match(/\n/g) || []).length;
                             if (keyText == " ")
-                                timer = timer + 150;
+                                timer = timer + timeSpace; //time space
                             else if (numberOfLineBreaks >= 1)
-                                timer = timer + 200;
+                                timer = timer + timeBreakLine; //time breakline
                             else
-                                timer = timer + 100;
+                                timer = timer + timeWriter; //time letter
                             contieneCallback(keyText, timer, function (respuesta2) {
                                 ejecutar(element, respuesta2);
                             });
@@ -112,7 +188,7 @@ function contieneCallback(cadena, timer, callBack) {
 }
 
 function ejecutar(element, keyText) {
-    var numberOfLineBreaks = (keyText.match(/\n/g) || []).length;
+    let numberOfLineBreaks = (keyText.match(/\n/g) || []).length;
     if (numberOfLineBreaks >= 1) {
         document.querySelector(element).innerHTML = document.querySelector(element).innerHTML + "<br />";
         return;
